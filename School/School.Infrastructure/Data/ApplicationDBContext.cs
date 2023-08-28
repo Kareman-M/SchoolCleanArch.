26224e407257e4867886b52298a69b07c27b3961
karemanm89 <karemanm89@gmail.com>
2023-08-28 08:54:09 +0200

    first commit

---
+++ School/School.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Data.Entites;
+
+namespace School.Infrastructure.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubjects { get; set; }
+    }
+}
